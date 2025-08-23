d4639da9fe12b6fbc0702e2e2477c0f9c9e1ce98
Christopher Plain <me@christopherplain.com>
2025-08-22 22:02:53 -0700

    Remove unused groups

--- lua/flexoki/groups/gitsigns.lua
+++
@@ -1,9 +0,0 @@
----@param colors flexoki.ThemeColors The color palette
----@return flexoki.HighlightGroups # Highlight groups for gitsigns.nvim
-return function(colors)
-    return {
-        GitSignsAdd = { fg = colors.green },
-        GitSignsChange = { fg = colors.yellow },
-        GitSignsDelete = { fg = colors.red },
-    }
-end
